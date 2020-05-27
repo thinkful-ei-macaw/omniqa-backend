1d78ca49fcbeec6bb3a833d8d499b7d0257be807
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-05-27 14:13:24 -0700

    created migration tables

---
+++ migrations/001.undo.create_tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS answer_upvotes;
+
