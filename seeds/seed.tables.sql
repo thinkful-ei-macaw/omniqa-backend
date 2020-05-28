35a5f059af7cfe004093d346248ab67d3d4881ec
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-05-28 10:40:08 -0700

    database and migrations are working

---
+++ seeds/seed.tables.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (id, name, username, password)
+VALUES (
+  1,
+  'Connor',
+  'ConnorOmni',
+  '$2a$12$ok8Ff1rVNPCrsGixA6cTxeNGWsbq.dEa0LeJcKdCLSM3.gc.0ct3iASDFasdf12!@'
+)
--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 /* eslint-disable no-console */
 const app = require('./app');
-const { NODE_ENV, PORT } = require('./config');
+const { NODE_ENV, PORT, DATABASE_URL } = require('./config');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL
+});
+
+app.set('db', db);
+
 
 app.listen(PORT, () => console.log(`Server listening in ${NODE_ENV} mode at http://localhost:${PORT}`));
