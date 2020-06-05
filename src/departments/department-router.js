f6920c31b5ef284ec9065eb8d7c022bacbcc3852
Alex Larrabee <nekotree556@gmail.com>
2020-06-05 13:36:43 -0700

    department routes and services added

--- src/departments/department-router.js
+++ src/departments/department-router.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const departmentRouter = express.Router();
-const {requireAuth} = require('../middleware/jwt-auth');
+const { requireAuth } = require('../middleware/jwt-auth');
 const DepartmentService = require('./department-service');
+
+
+departmentRouter
+  .route('/')
+  .get((req, res, next) => {
+    DepartmentService.getDepartmentList(req.app.get('db'))
+      .then(departments => {
+        res.json(departments);
+      })
+      .catch(next);
+  });
+
 
 departmentRouter
   .route('/:department_id')
   .get(requireAuth, (req, res, next) => {
     const departmentID = req.params.department_id;
     DepartmentService.getDepartmentName(req.app.get('db'), departmentID)
-      .then(department_name  => {
+      .then(department_name => {
         if (!department_name) {
           return res.status(404).json({
             error: {
