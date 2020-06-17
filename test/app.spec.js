33ae143dcb111e4ab365f4610f2f07f1f1e5f608
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-06-17 15:48:33 -0600

    Added request handling with 200 and Hello World

---
+++ test/app.spec.js
@@ -0,0 +1,11 @@
+const app = require('../src/app');
+
+describe.only('App', () => {
+  // default endpoint
+  describe('GET /', () => {
+    // happy test
+    it('responds with 200 containing "Hello, world!"', () => {
+      return supertest(app).get('/').expect(200, 'Hello, world!');
+    });
+  });
+});
