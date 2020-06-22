39c000e32381327a15f732dedf9f9d77c5e6c872
Jordan <jknox148@gmail.com>
2020-06-22 11:45:08 -0700

    removed lines 47-50

--- src/questions/question-router.js
+++ src/questions/question-router.js
@@ -44,11 +44,11 @@ questionRouter
       department: department_id,
       answered: false
     };
-    if (typeof department_id !== 'number') {
-      res.status(400).json({
-        error: 'Department ID must be a number'
-      });
-    }
+    // if (typeof department_id !== 'number') {
+    //   res.status(400).json({
+    //     error: 'Department ID must be a number'
+    //   });
+    // }
 
     for (const [key, value] of Object.entries(newQuestion)) {
       if (value == null) {
